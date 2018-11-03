f2b1e6400cbd30ef3e29612f38206635540df0e1
Joe <joe.is.ohara@gmail.com>
2018-11-03 17:00:02 +0000

    Fix decrypt method and add layer2

--- sharescript.py
+++ sharescript.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-import os,sys,argparse,tempfile,shutil
+import os, sys, argparse, tempfile, shutil
 import secretsharing as sss
 
-import jsonpickle # install via  "$ sudo pip install -U jsonpickle"
+import jsonpickle  # install via  "$ sudo pip install -U jsonpickle"
 
 from hashlib import sha256
 
-from passlib.hash import pbkdf2_sha256,argon2,sha512_crypt,sha1_crypt
+from passlib.hash import pbkdf2_sha256, argon2, sha512_crypt, sha1_crypt
 
 from random import randrange
 import json
@@ -15,7 +15,8 @@ import base64
 from Crypto.Cipher import AES
 from Crypto import Random
 
-def pxor(pwd,share):
+
+def pxor(pwd, share):
     '''
       XOR a hashed password into a Shamir-share
       1st few chars of share are index, then "-" then hexdigits
@@ -24,35 +25,37 @@ def pxor(pwd,share):
       we left pad the sha256(pwd) with zeros if the hexdigits are longer
       we left pad the output with zeros to the full length we xor'd
     '''
-    words=share.split("-")
-    hexshare=words[1]
-    slen=len(hexshare)
-    hashpwd=sha256(pwd).hexdigest()
-    hlen=len(hashpwd)
-    outlen=0
-    if slen<hlen:
-        outlen=slen
-        hashpwd=hashpwd[0:outlen]
-    elif slen>hlen:
-        outlen=slen
-        hashpwd=hashpwd.zfill(outlen)
+    words = share.split("-")
+    hexshare = words[1]
+    slen = len(hexshare)
+    hashpwd = sha256(pwd).hexdigest()
+    hlen = len(hashpwd)
+    outlen = 0
+    if slen < hlen:
+        outlen = slen
+        hashpwd = hashpwd[0:outlen]
+    elif slen > hlen:
+        outlen = slen
+        hashpwd = hashpwd.zfill(outlen)
     else:
-        outlen=hlen
-    xorvalue=int(hexshare, 16) ^ int(hashpwd, 16) # convert to integers and xor
-    paddedresult='{:x}'.format(xorvalue)          # convert back to hex
-    paddedresult=paddedresult.zfill(outlen)       # pad left
-    result=words[0]+"-"+paddedresult              # put index back
+        outlen = hlen
+    xorvalue = int(hexshare, 16) ^ int(hashpwd, 16)  # convert to integers and xor
+    paddedresult = '{:x}'.format(xorvalue)  # convert back to hex
+    paddedresult = paddedresult.zfill(outlen)  # pad left
+    result = words[0] + "-" + paddedresult  # put index back
     return result
+
 
 def newsecret(numbytes):
     '''
         let's get a number of pseudo-random bytes, as a hex string
     '''
-    binsecret=open("/dev/urandom", "rb").read(numbytes)
-    secret=binsecret.encode('hex')
+    binsecret = open("/dev/urandom", "rb").read(numbytes)
+    secret = binsecret.encode('hex')
     return secret
 
-def pwds_to_shares(pwds,k,numbytes):
+
+def pwds_to_shares(pwds, k, numbytes):
     '''
         Give a set of n passwords, and a threshold (k) generate a set
         of Shamir-like 'public' shares for those.
@@ -65,46 +68,51 @@ def pwds_to_shares(pwds,k,numbytes):
         is not for real use. With guessable passwords, the secret
         can be re-constructed!
     '''
-    n=len(pwds) # we're in k-of-n mode...
-    secret=newsecret(numbytes) # generate random secret
-    shares=sss.SecretSharer.split_secret(secret,k,n) # split secret
-    diffs=[] # diff the passwords and shares
-    for i in range(0,n):
-        diffs.append(pxor(pwds[i],shares[i]))
+    n = len(pwds)  # we're in k-of-n mode...
+    secret = newsecret(numbytes)  # generate random secret
+    shares = sss.SecretSharer.split_secret(secret, k, n)  # split secret
+    diffs = []  # diff the passwords and shares
+    for i in range(0, n):
+        diffs.append(pxor(pwds[i], shares[i]))
     return diffs
 
-def pwds_shares_to_secret(kpwds,kinds,diffs):
+
+def pwds_shares_to_secret(kpwds, kinds, diffs):
     '''
         take k passwords, indices of those, and the "public" shares and
         recover shamir secret
     '''
-    shares=[]
-    for i in range(0,len(kpwds)):
-        shares.append(str(pxor(kpwds[i],diffs[kinds[i]])))
-    secret=sss.SecretSharer.recover_secret(shares)
+    shares = []
+    for i in range(0, len(kpwds)):
+        shares.append(str(pxor(kpwds[i], diffs[kinds[i]])))
+    secret = sss.SecretSharer.recover_secret(shares)
     return secret
 
 
-unpad = lambda s: s[:-ord(s[len(s)-1:])]
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
 
 def decrypt(ciphertext, key):
-    dec = base64.b64decode(ciphertext)
-    iv = dec[:16]
-    cipher = AES.new(key,AES.MODE_CBC,iv) 
-    return unpad(cipher.decrypt(ciphertext[16:]))
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = base64.b64decode(ciphertext)
+    decrypted = cipher.decrypt(ciphertext)
+    return decrypted
+
 
 def encrypt(raw, key):
     raw = pad(raw)
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     return base64.b64encode(iv + cipher.encrypt(raw))
+
+
 BLOCK_SIZE = 16
 
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 
-
-with open("dec.txt","w+") as newF, open("layer1.broken") as broken,  open("00072_oharaj2.as5") as ballfile:
-    cracked = map(lambda x : x.split(':'),broken.read().splitlines())
+with open("dec.txt", "w+") as newF, open("layer1.broken") as broken, open("00072_oharaj2.as5") as ballfile:
+    cracked = map(lambda x: x.split(':'), broken.read().splitlines())
     ball = json.load(ballfile)
     hashes = ball["hashes"]
     shares = ball["shares"]
@@ -113,8 +121,7 @@ with open("dec.txt","w+") as newF, open("layer1.broken") as broken,  open("00072
     for hashpass in cracked:
         kinds.append(hashes.index(hashpass[0]))
         kpwds.append(hashpass[1])
-    secret = pwds_shares_to_secret(kpwds,kinds,shares)
-    print secret.zfill(32).decode('hex')
-    nextlev = decrypt(ball["ciphertext"],secret.zfill(32).decode('hex'))
+    secret = pwds_shares_to_secret(kpwds, kinds, shares)
+    nextlev = decrypt(ball["ciphertext"], secret.zfill(32).decode('hex'))
     print secret
-    newF.write(nextlev)
+    newF.write(str(nextlev))
